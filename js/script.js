f4e4f46ca4731a8a7f503e44ffe0c7e945d91819
Nikanora <37082879+Nikanorra@users.noreply.github.com>
2020-10-06 12:13:59 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 
+//в сайдбаре слайдер. Измеряем ширину картинки, по первой картинке задаем ширину остальных картинко в слайдере и по ширине картинок выравниваем блок с текстом
+$(window).load(function() {
+  let carouselCaptionWidth = $('#carouselSidebar .active img').width();
+  $('#carouselSidebar img').each(function(){
+    $(this).attr('width', carouselCaptionWidth); //остальные картинки по ширине приравниваем ширине первой
+  });
+  $('#carouselSidebar .carousel-caption-sitebar').css('max-width', carouselCaptionWidth + 'px');
+  $('#carouselSidebar .carousel-indicators').css('max-width', carouselCaptionWidth + 'px');
+  $('.sidebar .banner').css('max-width', carouselCaptionWidth + 'px');
+  $('.sidebar .about').css('max-width', carouselCaptionWidth + 'px');
+});
